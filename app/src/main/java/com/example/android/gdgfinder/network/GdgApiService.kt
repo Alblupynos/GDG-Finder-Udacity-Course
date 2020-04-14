24534fe6d8976b94af45fe799994d0c476446d58
Albina <alblupynos@gmail.com>
2020-04-14 21:53:25 +0300

    RTL Support. Content Description. Content Grouping.

--- app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
+++ app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
@@ -9,23 +9,25 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://developers.google.com/community/gdg/directory/"
+private const val BASE_URL = "https://developers.google.com/community/gdg/groups/"
+
 interface GdgApiService {
     @GET("directory.json")
     fun getChapters():
     // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
             Deferred<GdgResponse>
 }
+
 private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .baseUrl(BASE_URL)
-        .build()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
 
 object GdgApi {
-    val retrofitService : GdgApiService by lazy { retrofit.create(GdgApiService::class.java) }
+    val retrofitService: GdgApiService by lazy { retrofit.create(GdgApiService::class.java) }
 }
